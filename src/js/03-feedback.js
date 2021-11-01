5f26c887a8aba4cb26493924caa324f15053e6ed
Svit-l <lov.svit@gmail.com>
2021-11-01 03:26:17 +0200

    Задача 3 хранение данных формы в локальном хранилище

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
 
+// Отслеживай на форме событие input,
+// и каждый раз записывай в локальное хранилище объект с полями email и message,
+//   в которых сохраняй текущие значения полей формы.
+//   Пусть ключом для хранилища будет строка "feedback-form-state".
+// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд.
+// Для этого добавь в проект и используй библиотеку lodash.throttle.
+
+const feedbackForm = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(evt => {
+    let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+    persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+    persistedFilters[evt.target.name] = evt.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedFilters));
+    console.log(persistedFilters);
+  }, 500),
+);
+
+// При загрузке страницы проверяй состояние хранилища,
+// и если там есть сохраненные данные, заполняй ими поля формы.
+// В противном случае поля должны быть пустыми.
+
+function initForm() {
+  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters);
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+      feedbackForm.elements[name].value = value;
+    });
+  }
+}
+
+initForm();
+
+// При сабмите формы очищай хранилище и поля формы,
+// а также выводи объект с полями email, message и текущими их значениями в консоль.
+
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log('Отправляем форму');
+
+  let persistedFilters = localStorage.getItem(LOCALSTORAGE_KEY);
+  persistedFilters = JSON.parse(persistedFilters);
+  console.log(persistedFilters);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
